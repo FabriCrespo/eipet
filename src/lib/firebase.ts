8d4606f3b3ad32252e1a977a8f264daf1ead6f74
cbbeandresvargas <128827821+cbbeandresvargas@users.noreply.github.com>
2025-11-18 13:40:53 -0400

    firebase init

---
+++ src/lib/firebase.ts
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8WH9ZcvgDgNe-O9lswpyHHAW4s2Syr4c",
+  authDomain: "eipet-171a7.firebaseapp.com",
+  projectId: "eipet-171a7",
+  storageBucket: "eipet-171a7.firebasestorage.app",
+  messagingSenderId: "86684689322",
+  appId: "1:86684689322:web:339903e273ca8bc91ec1d3",
+  measurementId: "G-P6RF641PQH"
+};
